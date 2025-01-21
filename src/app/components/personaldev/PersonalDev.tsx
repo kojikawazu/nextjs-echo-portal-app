abcf391c1e09f6fff441ba2b7f134dd1fe497bda
kojikawazu <nanairo7.daydream21@gmail.com>
2025-01-21 22:05:32 +0900

    ポートフォリオ分追加

--- src/app/components/personaldev/PersonalDev.tsx
+++ src/app/components/personaldev/PersonalDev.tsx
@@ -12,6 +12,7 @@ import PersonalDevCard from '@/app/components/personaldev/atoms/PersonalDevCard'
  */
 const PersonalDev = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const introWebUrl = process.env.NEXT_PUBLIC_INTRO_WEB_URL || '/';
     const blogWebUrl = process.env.NEXT_PUBLIC_BLOG_WEB_URL || '/';
     const imageWebUrl = process.env.NEXT_PUBLIC_IMAGE_WEB_URL || '/';
     const textAiWebUrl = process.env.NEXT_PUBLIC_TEXT_AI_WEB_URL || '/';
@@ -19,6 +20,24 @@ const PersonalDev = () => {
         <MainLayout isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}>
             <main className="flex-grow container mx-auto px-6 py-12">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    <PersonalDevCard
+                        icon={<i className="fas fa-cog text-4xl text-[#3498db] mb-4"></i>}
+                        title="ポートフォリオ"
+                        description="私のブートフォリオを管理するWebアプリケーション"
+                        tech={[
+                            'React',
+                            'Next.js',
+                            'TypeScript',
+                            'Google Cloud',
+                            'Compute Engine',
+                            'Google Cloud Storage',
+                            'Google Cloud Functions',
+                            'GitHub Actions',
+                            'Resend',
+                        ]}
+                        url={introWebUrl}
+                        urlIcon={<i className="fas fa-arrow-right ml-2"></i>}
+                    />
                     <PersonalDevCard
                         icon={<i className="fas fa-cog text-4xl text-[#3498db] mb-4"></i>}
                         title="MyブログWebアプリケーション"
